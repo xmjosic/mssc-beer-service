807dd07453f25b494b050ff78d86ec215d9286d8
Marko Josić <xmjosic@gmail.com>
2022-07-20 22:13:59 +0200

    adding data model for beer service

---
+++ src/main/java/com/example/demo/model/BeerDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Builder
+public record BeerDto(
+        UUID id,
+        Integer version,
+        OffsetDateTime createdDate,
+        OffsetDateTime lastModifiedDate,
+        String beerName,
+        BeerStyleEnum beerStyle,
+        Long upc,
+        BigDecimal price,
+        Integer quantityOnHand
+) {
+}
---
+++ src/main/java/com/example/demo/model/BeerStyleEnum.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+public enum BeerStyleEnum {
+  LAGER,
+  PILSNER,
+  STOUT,
+  GOSE,
+  PORTER,
+  ALE,
+  WHEAT,
+  IPA,
+  PALE_ALE,
+  SAISON
+}
