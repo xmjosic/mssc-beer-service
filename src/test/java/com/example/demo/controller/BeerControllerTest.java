b3d2c952af5909b46642e12f244b148b65baf20b
Marko Josić <xmjosic@gmail.com>
2022-07-20 22:36:36 +0200

    adding beer service controller test

---
+++ src/test/java/com/example/demo/controller/BeerControllerTest.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.BeerDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+  @Autowired private ObjectMapper objectMapper;
+
+  @Test
+  void getBeer() throws Exception {
+    mockMvc
+        .perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  void createBeer() throws Exception {
+    BeerDto beerDto = BeerDto.builder().build();
+    String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+    mockMvc
+        .perform(post("/api/v1/beer").contentType(MediaType.APPLICATION_JSON).content(beerDtoJson))
+        .andExpect(status().isCreated());
+  }
+
+  @Test
+  void updateBeer() throws Exception {
+    BeerDto beerDto = BeerDto.builder().build();
+    String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+    mockMvc
+        .perform(
+            put("/api/v1/beer/" + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+        .andExpect(status().isNoContent());
+  }
+}
