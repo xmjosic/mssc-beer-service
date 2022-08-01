e5744bfd0a234e6cccd3bda3d94528d0a4c24fe0
Marko Josić <xmjosic@gmail.com>
2022-08-01 22:42:51 +0200

    added beer repository

---
+++ src/main/java/com/example/demo/repository/BeerRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {}
